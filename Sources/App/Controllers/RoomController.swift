50eceb274d728c34bbaef8a55e511d3a8616e946
ccr <sis.ccr@gmail.com>
2021-10-03 07:17:55 +0545

    adding add room api

--- Sources/App/Controllers/RoomController.swift
+++ Sources/App/Controllers/RoomController.swift
@@ -11,76 +11,50 @@ import Vapor
 
 struct RoomController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
-
+        
     }
-
+    
     func index(req: Request) throws -> EventLoopFuture<[Room]> {
         return Room.query(on: req.db).all()
     }
     
+    //    func create(req: Request) throws -> EventLoopFuture<Room> {
+    //        struct Entity: Content {
+    //                var images: File
+    //        }
+    //        let room = try req.content.decode(Room.self)
+    //        let file = try req.content.decode(Entity.self)
+    //        let image = file.images
+    //        let uploadPath = req.application.directory.publicDirectory + "uploads/"
+    //        let filename = "\(Date().timeIntervalSince1970)_" + image.filename.replacingOccurrences(of: " ", with: "")
+    //
+    //        return req.fileio.writeFile(
+    //            image.data,
+    //            at:  uploadPath + filename
+    //        ).flatMap {
+    //            room.vimages = [filename]
+    //           return room.save(on: req.db).map {
+    //            room
+    //           }
+    //        }
+    //    }
+    
     func create(req: Request) throws -> EventLoopFuture<Room> {
         struct Entity: Content {
-                var images: File
+            var images: [File]
         }
+        let uploadPath = req.application.directory.publicDirectory + "uploads/"
+        
         let room = try req.content.decode(Room.self)
         let file = try req.content.decode(Entity.self)
-        let image = file.images
-        let uploadPath = req.application.directory.publicDirectory + "uploads/"
-        let filename = "\(Date().timeIntervalSince1970)_" + image.filename.replacingOccurrences(of: " ", with: "")
-        
-        let uploadFutures = input.files
-                .filter { $0.data.readableBytes > 0 }
-                .map { file -> EventLoopFuture<UploadedFile> in
-                    let fileName = prefix + file.filename
-                    let path = app.directory.publicDirectory + fileName
-                    let isImage = ["png", "jpeg", "jpg", "gif"].contains(file.extension?.lowercased())
-                    
-                    return req.application.fileio.openFile(path: path,
-                                                           mode: .write,
-                                                           flags: .allowFileCreation(posixMode: 0x744),
-                                                           eventLoop: req.eventLoop)
-                        .flatMap { handle in
-                            req.application.fileio.write(fileHandle: handle,
-                                                         buffer: file.data,
-                                                         eventLoop: req.eventLoop)
-                                .flatMapThrowing { _ in
-                                    try handle.close()
-                                    return UploadedFile(url: fileName, isImage: isImage)
-                                }
-                            
-                        }
-                }
-        
-        return req.fileio.writeFile(
-            image.data,
-            at:  uploadPath + filename
-        ).flatMap {
-            room.vimages = [filename]
-           return room.save(on: req.db).map {
-            room
-           }
+        return file.images.map { file -> EventLoopFuture<String> in
+            let filename = "\(Date().timeIntervalSince1970)_" + file.filename.replacingOccurrences(of: " ", with: "")
+            return req.fileio.writeFile(file.data, at: uploadPath + filename ).map { filename }
+            
+        }.flatten(on: req.eventLoop).map { filenames in
+            room.vimages = filenames
+        }.flatMap { _ in
+            return room.save(on: req.db).map {room}
         }
-//      return  req.fileio.writeFile(file.data, at: path).map {Room()}
-     
-//        print(images?.count)
-        
-//            return room.create(on: req.db).map { room }
     }
-
-//    func create(req: Request) throws -> EventLoopFuture<Todo> {
-//        print(req.body)
-//        print(req.content)
-//        print(req.method)
-//        print(req.parameters)
-//        let todo = try req.content.decode(Todo.self)
-//        print(todo.title)
-//        return todo.save(on: req.db).map { todo }
-//    }
-//
-//    func delete(req: Request) throws -> EventLoopFuture<HTTPStatus> {
-//        return Todo.find(req.parameters.get("todoID"), on: req.db)
-//            .unwrap(or: Abort(.notFound))
-//            .flatMap { $0.delete(on: req.db) }
-//            .transform(to: .ok)
-//    }
 }
