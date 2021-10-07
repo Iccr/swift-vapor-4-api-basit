0fa7642d29b0fa21670aab1532f74fe5c890b184
ccr <sis.ccr@gmail.com>
2021-10-07 17:09:10 +0545

    added error inside error key

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,6 +4,7 @@ import Leaf
 import Vapor
 import JWT
 
+import Foundation
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
@@ -15,6 +16,10 @@ public func configure(_ app: Application) throws {
     app.views.use(.leaf)
     
     let file = FileMiddleware(publicDirectory: app.directory.publicDirectory)
+
+    app.middleware = .init()
+    app.middleware.use(MyErrorMiddleware())
+    
     app.middleware.use(file)
     app.routes.defaultMaxBodySize = "10mb"
     app.jwt.signers.use(.hs256(key: Env.jwtSecret))
@@ -87,11 +92,48 @@ func seed(_ db: Database)  {
             _ =   [b1, b2].create(on: db)
         }
     }
-    
-    
-   
-  
-   
-    
-    
+     
 }
+
+
+class MyErrorMiddleware: Middleware {
+    internal struct ErrorResponse: Codable {
+        /// Always `true` to indicate this is a non-typical JSON response.
+        /// The reason for the error.
+        var message: String
+    }
+    
+    func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
+        
+        return next.respond(to: request).flatMapErrorThrowing { error in
+            var reason: String
+            var status: HTTPResponseStatus
+            var headers: HTTPHeaders
+            switch error {
+                case let abort as AbortError:
+                    // this is an abort error, we should use its status, reason, and headers
+                    reason = abort.reason
+                    status = abort.status
+                    headers = abort.headers
+            
+                default:
+                    // not an abort error, and not debuggable or in dev mode
+                    // just deliver a generic 500 to avoid exposing any sensitive error info
+                    reason = "Something went wrong."
+                    status = .internalServerError
+                    headers = [:]
+//                    source = nil
+            }
+            
+            let response = Response(status: status, headers: headers)
+            
+            let errorResponse = ErrorResponse( message: reason)
+//            json = try JSONEncoder().encode(["error": result.message])
+            response.body = try .init(data: JSONEncoder().encode(["error": errorResponse] ))
+            response.headers.add(name: .contentType, value: "application/json")
+            return response
+            
+        }
+    }
+}
+
