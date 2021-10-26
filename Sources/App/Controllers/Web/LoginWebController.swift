5884f964c9734a08f42f98992d85232ef2d58371
shishir sapkota <sis.ccr@gmail.com>
2021-10-26 16:33:07 +0545

    added sign out

--- Sources/App/Controllers/Web/LoginWebController.swift
+++ Sources/App/Controllers/Web/LoginWebController.swift
@@ -13,11 +13,19 @@ import ImperialCore
 class LoginWebController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let route = routes.grouped("login")
-        routes.get(use: signIn)
+        route.get(use: signIn)
+        
+        let logOutRoute = routes.grouped("logout")
+        logOutRoute.get( use: signOut)
         
     }
     
     func signIn(req: Request) -> EventLoopFuture<View> {
         return req.view.render("login")
+    }
+    
+    func signOut(req: Request) -> EventLoopFuture<Response> {
+        req.auth.logout(User.self)
+        return req.eventLoop.makeSucceededFuture(req.redirect(to: "/"))
     }
 }
