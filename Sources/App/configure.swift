a8e3dbc78624fd56bb516b3a88d74b6c4a9b00f0
shishir sapkota <sis.ccr@gmail.com>
2022-01-02 17:54:20 +0545

    allowed cors

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -10,7 +10,16 @@ import Foundation
 public func configure(_ app: Application) throws {
     
     let file = FileMiddleware(publicDirectory: app.directory.publicDirectory)
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    // cors middleware should come before default error middleware using `at: .beginning`
+
     app.middleware = .init()
+    app.middleware.use(cors, at: .beginning)
 //    app.middleware.use(EnsureApiDomainMiddleware())
     app.middleware.use(MyErrorMiddleware())
     
